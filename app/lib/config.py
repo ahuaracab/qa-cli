dd6269e980a83d47a71a5cfb8dbdb6302a616b75
John Montero <jmonteroc@gmail.com>
2018-07-12 11:08:01 -0500

    reordenar las variables del config

--- app/lib/config.py
+++ app/lib/config.py
@@ -3,7 +3,10 @@ import yaml
 
 def get_config():
     config = {
-        "vesion": "0.1",
-        "appName": "QA Command Line -- Tools"
+        "author": "QA Team - UND",
+        "app":{
+            "name": "QA Command Line -- Tools",
+            "vesion": "0.0.1"
+        }
     }
     return config
